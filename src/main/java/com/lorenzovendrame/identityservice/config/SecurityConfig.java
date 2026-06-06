99257c9d40608c747406887c0053873e483be3b0
Lorenzo <lorenzovendrame@hotmail.com>
2026-06-06 17:59:28 -0300

    Add PreAuthorize in endpoints

--- src/main/java/com/lorenzovendrame/identityservice/config/SecurityConfig.java
+++ src/main/java/com/lorenzovendrame/identityservice/config/SecurityConfig.java
@@ -2,14 +2,18 @@ package com.lorenzovendrame.identityservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
@@ -21,9 +25,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                );
+                        .requestMatchers(HttpMethod.POST, "/users").permitAll() // Cadastro público
+                        .anyRequest().authenticated()
+                )
+                // INJETA O SEU FILTRO DO GATEWAY antes do filtro padrão de autenticação do Spring
+                .addFilterBefore(new GatewayHeaderAuthenticationFilter(),
+                        org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 }
